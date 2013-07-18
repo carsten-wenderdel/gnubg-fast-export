695ce1debb48b6ae9679a6f504ff05609f8a10fb
mdpetch <mdpetch>
2013-07-18 02:01:24 +0000

    Fix stack issues that cause AVX Win32 builds to segfault when optimizations are used

--- dbprovider.c
+++ dbprovider.c
@@ -19,14 +19,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: dbprovider.c,v 1.39 2013/07/10 22:35:13 mdpetch Exp $
+ * $Id: dbprovider.c,v 1.40 2013/07/18 02:01:23 mdpetch Exp $
  */
 
 #include "config.h"
 #include "gnubgmodule.h"
 
 #include "backgammon.h"
-#include <stdlib.h>
 #include <glib/gstdio.h>
 #include <string.h>
 #include "dbprovider.h"
