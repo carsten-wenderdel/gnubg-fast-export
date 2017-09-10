bf1b3c4eff828879340d379125eb02055717de0a
plm <plm>
2017-09-10 11:57:41 +0000

    Fix possible integer overflow

--- openurl.c
+++ openurl.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: openurl.c,v 1.30 2017/09/10 11:53:38 plm Exp $
+ * $Id: openurl.c,v 1.31 2017/09/10 11:57:41 plm Exp $
  */
 #include "config.h"
 #include <stdio.h>
@@ -72,7 +72,7 @@ OpenURL(const char *szURL)
         ptrdiff_t win_error;
         gchar *url = g_filename_to_uri(szURL, NULL, NULL);
         win_error = (ptrdiff_t) ShellExecute(NULL, TEXT("open"), url ? url : szURL, NULL, ".\\", SW_SHOWNORMAL);
-        if (win_error < 33)
+        if (win_error >= 0 && win_error < 33)
             outputerrf(_("Failed to perform default action on " "%s. Error code was %d"), url, (int)win_error);
         g_free(url);
         return;
