b99eecab65f0f16f16a283a5f0d125929eb27281
plm <plm>
2019-12-24 09:49:10 +0000

    Add proper copyright attributions as suggested by FSF.

--- board3d/BoardDimensions.h
+++ board3d/BoardDimensions.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2019 Jon Kinsey <jonkinsey@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * $Id: BoardDimensions.h,v 1.2 2019/12/24 09:49:10 plm Exp $
+ */
+
 /* All the board element sizes - based on base_unit size */
 
 /* Widths */
