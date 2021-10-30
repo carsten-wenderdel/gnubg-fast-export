0307fb45ea3c633c0218b0756254cbd8d0edf5f5
plm <plm>
2021-10-30 14:16:23 +0000

    Avoid using reserved identifiers in enum definition

--- sound.h
+++ sound.h
@@ -15,13 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: sound.h,v 1.26 2020/01/05 19:51:24 plm Exp $
+ * $Id: sound.h,v 1.27 2021/10/30 14:16:23 plm Exp $
  */
 
 #ifndef SOUND_H
 #define SOUND_H
 
-typedef enum _gnubgsound {
+typedef enum {
     /* start & exit of gnubg */
     SOUND_START = 0,
     SOUND_EXIT,
