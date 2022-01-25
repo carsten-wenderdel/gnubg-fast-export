33cc3063e0bca352393260c39026f4c681a97f0f
plm <plm>
2022-01-25 20:17:17 +0000

    Avoid dead store

--- sgf.c
+++ sgf.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: sgf.c,v 1.171 2021/11/14 21:41:22 plm Exp $
+ * $Id: sgf.c,v 1.172 2022/01/25 20:17:17 plm Exp $
  */
 
 #include "config.h"
@@ -1620,7 +1620,7 @@ WriteEscapedString(FILE * pf, char *pch, int fEscapeColons)
             break;
         }
 
-    *pc++ = 0;
+    *pc = 0;
 
     fputs(sz, pf);
     g_free(sz);
