7aa4196877f4c4d23123b3a939a89472da8d5fd5
plm <plm>
2022-04-21 20:50:21 +0000

    Remove duplicate LegacyEndAA() call, added in rev. 1.7

--- board3d/DrawOGL.c
+++ board3d/DrawOGL.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: DrawOGL.c,v 1.23 2022/02/26 20:49:04 plm Exp $
+ * $Id: DrawOGL.c,v 1.24 2022/04/21 20:50:21 plm Exp $
  */
 
 #include "config.h"
@@ -727,7 +727,6 @@ static void MAApoints(const renderdata* prd)
 		tuv = 0;
 
 	LegacyStartAA(1.0f);
-	LegacyEndAA();
 
 	drawPointLegacy(prd, tuv, 0, 0, 1);
 	drawPointLegacy(prd, tuv, 0, 1, 1);
